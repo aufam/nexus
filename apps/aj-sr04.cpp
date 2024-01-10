d449becc28110f230012c1e762d9c0aacb157685
aufam <aufamuhammad715@gmail.com>
2024-01-10 18:19:58 +0700

    adding aj-sr04

---
+++ apps/aj-sr04.cpp
@@ -0,0 +1,114 @@
+#include "nexus/abstract/device.h"
+#include "nexus/abstract/listener.h"
+#include "nexus/serial/serial.h"
+#include "nexus/http/server.h"
+
+#include "nexus/tools/filesystem.h"
+#include "nexus/tools/options.h"
+#include "nexus/tools/on_kill.h"
+#include "nexus/tools/timestamp.h"
+#include "nexus/tools/to_string.h"
+#include "nexus/tools/json.h"
+#include <iostream>
+#include <etl/keywords.h>
+
+using namespace std::literals;
+
+class AJSR04 : virtual public nexus::abstract::Device, virtual public nexus::serial::Serial {
+public:
+    AJSR04(std::string port) : nexus::serial::Serial::Serial(port, B9600) {}
+    
+    std::vector<uint8_t> decode(nexus::byte_view buffer) const noexcept override {
+        return buffer.len() >= 10 
+            && buffer[0] == 'G' && buffer[1] == 'a' && buffer[2] == 'p' && buffer[3] == '=' 
+            && buffer[-3] == 0x0d && buffer[-2] == 0x0a && buffer[-1] == 0x0d 
+            ? std::vector<uint8_t>(buffer.begin() + 4, buffer.end())
+            : std::vector<uint8_t>();
+    }
+
+    void update() override {
+        uint8_t dummy[] = {0x01};
+        send(nexus::byte_view{dummy, sizeof(dummy)});
+        auto response = receiveText();
+        distance = etl::string_view(response.data(), response.size()).to_float() / 1000.f;
+        std::cout << "Distance: " << distance << " m" << std::endl;
+    }
+
+    std::string path() const override { 
+        return "/aj-sr04"; 
+    }
+    std::string json() const override { 
+        return nexus::tools::json_concat(
+            nexus::serial::Serial::json(), 
+            "{\"distance\": " + nexus::tools::to_string(distance, 3) + "}"
+        ); 
+    }
+    using nexus::serial::Serial::post;
+    using nexus::serial::Serial::patch;
+
+    float distance;
+};
+
+int main(int argc, char* argv[]) {
+    var serial_port = std::string("auto");
+    var host = std::string("localhost");
+    var port = 5000;
+    var page = std::string(nexus::tools::parent_path(__FILE__) / "aj-sr04.html");
+
+    nexus::tools::execute_options(argc, argv, {
+        {'s', "serial-port", required_argument, [&] (const char* arg) { 
+            serial_port = arg; 
+        }},
+        {'H', "host", required_argument, [&] (const char* arg) { 
+            host = arg; 
+        }},
+        {'p', "port", required_argument, [&] (const char* arg) { 
+            port = std::atoi(arg); 
+        }},
+        {'P', "page", required_argument, [&] (const char* arg) { 
+            page = arg; 
+        }},
+        {'h', "help", no_argument, [] (const char*) {
+            std::cout << "AJ-SR04M Interface\n";
+            std::cout << "Options:\n";
+            std::cout << "-s, --serial-port Specify the serial port. Default = auto\n";
+            std::cout << "-H, --host        Specify the server host. Default = localhost\n";
+            std::cout << "-p, --port        Specify the server port. Default = 5000\n";
+            std::cout << "-P, --page        Specify the HTML page to serve in the main path. Default = aj-sr04.index\n";
+            std::cout << "-h, --help        Print help\n";
+            exit(0);
+        }},
+    });
+    
+    var listener = nexus::abstract::Listener();
+    listener.interval = 1s;
+    listener.add(std::make_unique<AJSR04>(serial_port));
+
+    var server = nexus::http::Server();
+    server.add(listener[0]);
+    server.set_logger([] (const httplib::Request& request, const httplib::Response& response) { 
+        std::cout << 
+            nexus::tools::current_time.get() << " " << 
+            request.remote_addr << " " << 
+            request.method << " " << 
+            request.path << " " << 
+            request.version << " " << 
+            response.status << std::endl;
+    }); 
+    server.Get("/", [&page] (const httplib::Request&, httplib::Response& response) {
+        try {
+            response.set_content(nexus::tools::read_file(page), "text/html");
+            response.status = 200;
+        } catch (const std::exception& e) {
+            response.set_content(e.what(), "text/plain");
+            response.status = 500;
+        }
+    });
+
+    nexus::tools::on_kill([&server] { server.stop(); });
+
+    std::cout << "Server is running on http://" << host << ":" << port << "/" << std::endl;
+    server.listen(host, port);
+    std::cout << std::endl;
+    return 0;
+}
---
+++ apps/aj-sr04.py
@@ -0,0 +1,109 @@
+import py_nexus
+import json
+import os
+import time
+import datetime
+
+
+class AJSR04(py_nexus.Device):
+    def __init__(self, serial_port: str):
+        super().__init__()
+        self.distance = 0
+        self.serial = py_nexus.Serial(port=serial_port, speed=py_nexus.B9600)
+
+        @self.serial.decode_override
+        def _(buffer):
+            return buffer[4:] \
+                if len(buffer) >= 10 \
+                    and buffer[0] == ord('G') and buffer[1] == ord('a') and buffer[2] == ord('p') and buffer[3] == ord('=') \
+                    and buffer[-1] == 0x0d \
+                else []
+    
+    def update(self):
+        try:
+            self.serial.send(text='1')
+            response: str = self.serial.receiveText()
+            # Find the index 
+            equals_index = response.find('=')
+            mm_index = response.find('mm')
+
+            # Extract the substring between '=' and 'm'
+            value_str = response[equals_index + 1:mm_index]
+
+            # Convert the extracted substring to a float
+            self.distance = float(value_str) / 1000
+        except Exception as e:
+            self.distance = 0
+        finally:
+            print(f'Distance: {self.distance} m')
+    
+    def path(self) -> str:
+        return '/aj-sr04'
+
+    def json(self) -> str:
+        data1 = json.loads(self.serial.json)
+        data2 = json.loads('{"distance": ' + str(self.distance) + '}')
+        return json.dumps({**data1, **data2})
+    
+    def post(self, method_name: str, json_request: str) -> str:
+        return self.serial.post(method_name, json_request)
+    
+    def patch(self, json_request: str) -> str:
+        return self.serial.patch(json_request)
+    
+
+def main(host, port, page, serial_port):
+    ajsr04 = AJSR04(serial_port=serial_port)
+    listener = py_nexus.Listener()
+    listener.add(ajsr04)
+    listener.interval = datetime.timedelta(seconds=1)
+
+    server = py_nexus.HttpServer()
+    server.add(ajsr04)
+    
+    @server.Get('/')
+    def _(request, response):
+        try:
+            with open(page, 'r') as file:
+                file_content = file.read()
+            response.set_content(file_content, 'text/html')
+            response.status = 200
+        except Exception as e:
+            response.set_content(str(e), 'text/plain')
+            response.status = 500
+
+        return response
+    
+    @server.logger
+    def _(request, response):
+        print(f'{request.remote_addr} {request.method} {request.path} {request.version} {response.status} {response.body}')
+    
+    server.listen(host=host, port=port)
+    print(f"Server is running on http://{host}:{port}/")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nCtrl+C pressed. Exiting...")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    
+    server.stop()
+    listener.stop()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    parser = argparse.ArgumentParser(description='AJ-SR04M Interface')
+
+    parser.add_argument('-H', '--host', type=str, default='localhost', help='Specify the server host')
+    parser.add_argument('-p', '--port', type=int, default=5000, help='Specify the server port')
+    parser.add_argument('-P', '--page', type=str, default=script_directory + '/ajsr04.html', help='Specify the HTML page')
+    parser.add_argument('-s', '--serial-port', type=str, default='auto', help='Specify the serial port')
+
+    args = parser.parse_args()
+    main(**vars(args))
+
